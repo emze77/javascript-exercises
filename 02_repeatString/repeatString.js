afd373d82f4e2a96b1d97026bd7ba3b3af827a5b
emze <kontakt@marcus-sperber.de>
2025-02-19 19:35:22 +0100

    Exc.2: passes 4 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+    // if (num > 0) {
+    //     result = string;
+    // } else if (num < 0) {
+    //     result = "ERROR";
+    // }
 
-};
+        for (i = 0; i <= (num - 1); i++) {
+            result += string ;
+            console.log(string)
+        }
+        return result;
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
