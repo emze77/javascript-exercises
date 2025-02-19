a15583b58e791f62ec3a80e81b26037a01db6aaf
emze <kontakt@marcus-sperber.de>
2025-02-19 21:42:13 +0100

    Exc.3: first test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const len = word.length;
+    let reversed = "";
+    for (let i = 1; i <= len; i++) {
+        reversed += word.at(-i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
