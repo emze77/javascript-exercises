70b1364d27963e341809c46a7e0b67b3b89bd498
emze <kontakt@marcus-sperber.de>
2025-02-20 21:51:46 +0100

    online-liner passed!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,5 @@
 const leapYears = function(year) {
-    let test1 = false;
-    let test2 = false;
-    if ((year % 4 === 0) && (year % 100 != 0)) test1 = true;
-    if ((test1 === true) || (year % 400 === 0)) test2 = true;
-    return test2;
+    return (((year % 4 === 0) && (year % 100 != 0)) || (year % 400 === 0));
 };
 
 // Do not edit below this line
