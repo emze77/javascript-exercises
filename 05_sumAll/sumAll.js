faabb48083a6c46794d6645de80b8b5a82699d6a
emze <kontakt@marcus-sperber.de>
2025-02-20 19:44:11 +0100

    Exc.5: 4 Test pass, but not beautiful

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+function checkLegality (el) {
+    if (isNaN(el) || 
+    (el < 0) ||
+    ((el % 1) != 0))     
+    return 0;
+}
 
+const sumAll = function(arg1, arg2) {
+    if ((checkLegality(arg1) === 0) ||
+        (checkLegality(arg2) === 0))
+        return "ERROR";
+
+    const higherNumber = Math.max(arg1, arg2);
+    const lowerNumber = Math.min(arg1, arg2);
+    let sum = 0;
+    for (let i = lowerNumber; i <= higherNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
