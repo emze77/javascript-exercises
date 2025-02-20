e83bfdb537d59bd7a4dcb6a6ea0678d02b31ea00
emze <kontakt@marcus-sperber.de>
2025-02-20 01:54:58 +0100

    Exc.4 first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...deleteParameters) {
+    let delPara = [];
+    delPara = deleteParameters;
+    const paraNum = deleteParameters.length;
+    for (let i = 0; i < paraNum; i++) {
+       let position = inputArray.indexOf(delPara[i]);
+       inputArray.splice(position, 1);
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
